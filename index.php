90ad138ddb73d9d30e2266401d73f3809337cce1
loscai <christiancolombo2k5@gmail.com>
2025-04-23 15:41:21 +0200

    sistemazione grafica e setup pagina funzionale per l'aggiunta di un evento

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CALENDARIO</title>
     <link rel="stylesheet" href="./style/style.css">
 </head>
-
 <body>
-    <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FRome&title=CALENDARIO%20SCOLASTICO%20CHRISTIAN&src=Y2hyaXN0aWFuY29sb21ibzJrNUBnbWFpbC5jb20&src=aXQuaXRhbGlhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%230B8043"
-        style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <div class="container">
+        <iframe
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FRome&title=CALENDARIO%20SCOLASTICO%20CHRISTIAN&src=Y2hyaXN0aWFuY29sb21ibzJrNUBnbWFpbC5jb20&src=aXQuaXRhbGlhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%230B8043"
+            style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no">
+        </iframe>
+        <br>
+        <button>INSERISCI NUOVO EVENTO</button>
+    </div>
 </body>
-
 </html>
