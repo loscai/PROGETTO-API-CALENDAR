733e89c1f65537c7274649883c25fbdd325715cb
loscai <christiancolombo2k5@gmail.com>
2025-04-23 15:45:12 +0200

    creazione pagina per inserire i dati dell'evento

---
+++ preparaEvento.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SELEZIONA PARAMETRI EVENTO</title>
+</head>
+<body>
+    TITOLO: <input type="text" id="titolo" name="titolo"><br><br>
+    DESCRIZIONE: <input type="text" id="descrizione" name="descrizione"><br><br>
+    DATA INIZIO: <input type="date" id="data_inizio" name="data_inizio"><br><br>
+    DATA FINE: <input type="date" id="data_fine" name="data_fine"><br><br>
+    ORA INIZIO: <input type="time" id="ora_inizio" name="ora_inizio"><br><br>
+    ORA FINE: <input type="time" id="ora_fine" name="ora_fine"><br><br>
+    <button>INVIA</button>
+</body>
+</html>
