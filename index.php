6e3c33dfb476127a7ec372e0ec8b12704572da22
loscai <christiancolombo2k5@gmail.com>
2025-05-13 11:33:39 +0200

    aggiornato iframe

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,16 @@
     <link rel="stylesheet" href="./style/style.css">
     <script src="./js/script.js"></script>
 </head>
+
 <body>
     <div class="container">
         <iframe
-            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FRome&title=CALENDARIO%20SCOLASTICO%20CHRISTIAN&src=Y2hyaXN0aWFuY29sb21ibzJrNUBnbWFpbC5jb20&src=aXQuaXRhbGlhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%230B8043"
-            style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no">
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FRome&src=Y2hyaXN0aWFuY29sb21ibzJrNUBnbWFpbC5jb20&src=aXQuaXRhbGlhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%230B8043"
+            style="border-width:0" width="800" height="600" frameborder="0" scrolling="no">
         </iframe>
         <br>
         <button onclick="goToPreparaEvento()">INSERISCI NUOVO EVENTO</button>
     </div>
 </body>
+
 </html>
